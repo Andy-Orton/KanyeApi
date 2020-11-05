670852d8fa68df828d05d76fcd0e01c07d5cd7d2
Andy-Orton <andyorton13@gmail.com>
2020-11-05 14:14:45 -0700

    Initial commit

---
+++ src/test/java/com/AndyWo/TwitterKanyeAPI/TwitterKanyeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AndyWo.TwitterKanyeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterKanyeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
